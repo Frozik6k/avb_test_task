5b09f470d5d5e23176616a4ab4bc0ce1936b403f
Frozi <Frozik6k@yandex.ru>
2025-08-17 00:53:57 +0500

    временно сохранить, перед изменениями конфигураций

---
+++ config-server/src/main/java/ru/Frozik6k/configserver/ConfigServerApplication.java
@@ -0,0 +1,9 @@
+package ru.Frozik6k.configserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+}
--- user-service/src/main/java/ru/Frozik6k/userservice/UserServiceApplication.java
+++ user-service/src/main/java/ru/Frozik6k/userservice/UserServiceApplication.java
@@ -3,6 +3,7 @@ package ru.Frozik6k.userservice;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.Frozik6k.userservice.client.CompanyDto;
@@ -10,6 +11,7 @@ import ru.Frozik6k.userservice.client.CompanyDto;
 @SpringBootApplication
 @EnableFeignClients
 @OpenAPIDefinition
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
