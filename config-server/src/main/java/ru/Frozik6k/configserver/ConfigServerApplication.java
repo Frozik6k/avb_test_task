7b029e45aea14f23e7ce4cceb17530723c517280
Frozi <Frozik6k@yandex.ru>
2025-08-17 10:03:16 +0500

    временно сохранить, перед изменениями конфигураций

--- config-server/src/main/java/ru/Frozik6k/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/ru/Frozik6k/configserver/ConfigServerApplication.java
@@ -1,9 +1,13 @@
 package ru.Frozik6k.configserver;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
