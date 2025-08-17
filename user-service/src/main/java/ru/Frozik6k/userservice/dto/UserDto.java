6a4c51e73129dc2032e97970d88adb115e6393e1
Frozi <Frozik6k@yandex.ru>
2025-08-17 18:08:40 +0500

    bug-fix.

--- user-service/src/main/java/ru/Frozik6k/userservice/dto/UserDto.java
+++ user-service/src/main/java/ru/Frozik6k/userservice/dto/UserDto.java
@@ -1,6 +1,7 @@
 package ru.Frozik6k.userservice.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.Frozik6k.userservice.model.User;
@@ -12,21 +13,23 @@ public class UserDto {
     private String name;
     private String surname;
     private String tel;
+    @JsonProperty("id_company")
+    private Long idCompany;
 
     public UserDto(User user) {
         id = user.getId();
         name = user.getName();
         surname = user.getSurname();
         tel = user.getTel();
+        idCompany = user.getIdCompany();
     }
     @JsonIgnore
     public User getUser() {
         User user = new User();
-        user.setId(id);
         user.setName(name);
         user.setSurname(surname);
         user.setTel(tel);
-        user.setIdCompany(0L);
+        user.setIdCompany(idCompany);
         return user;
     }
 
